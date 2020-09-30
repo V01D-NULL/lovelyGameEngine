a3fbb7d52c146dffdb7421f6457acf3a67fcfbf0
V01D <58613685+V01D-NULL@users.noreply.github.com>
2020-09-30 19:07:46 +0000

    Add files via upload

--- src/main.cxx
+++ src/main.cxx
@@ -1,6 +1,12 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main() {
-    
+    if (!glfwInit())
+    {
+        std::cout << "Error\n";
+    }
+    else
+        std::cout << "Success\n";
 }
