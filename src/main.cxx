718afe23523c74e9e84cd6436c5ed5d0a4a5ca5a
V01D <timster23@protonmail.com>
2020-10-03 12:56:47 +0000

    .

--- src/main.cxx
+++ src/main.cxx
@@ -6,21 +6,15 @@
 #include "core/logging/logging.h"
 #include "interface/window.h"
 
-//test
-
 int main() {
     lovely::logging::logger::info("Initializing glfw...", NULL);
     
     if (!glfwInit())
     {
-//         lovely::errorHanlder::getGLError();
         lovely::logging::logger::err("Could not initialize glfw.", NULL);
         return -1;
     }
     else
         lovely::logging::logger::info("Success", NULL);
     
-    std::string msg = "test";
-    std::string conv = std::to_string(10);
-    lovely::logging::logger::err(msg.c_str(), conv.c_str(), NULL);
 }
