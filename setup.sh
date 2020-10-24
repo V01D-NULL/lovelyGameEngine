6486b89dab1b73c382d20181e8205fbfb37aefa7
V01D <timster23@protonmail.com>
2020-10-24 14:09:05 +0000

    fixed installer script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-echo "Installing packages"
-sudo pacman -Sy premake p7zip --noconfirm
-sudo pip install cmake
+#echo "Installing packages"
+#sudo pacman -Sy premake p7zip --noconfirm
+#sudo pip install cmake
 
 cd ../
 
@@ -8,23 +8,26 @@ echo "Cloning SOIL2 repo"
 git clone "https://github.com/SpartanJ/SOIL2.git"
 
 echo "Unpacking zip files"
-7z x lovelyGameEngine/ZIP_FILES/glm-0.9.9.8.zip .
-7z x lovelyGameEngineZIP_FILES/glfw-3.2.1.zip .
+cp lovelyGameEngine/ZIP_FILES/glm-0.9.9.8.zip .
+cp lovelyGameEngine/ZIP_FILES/glfw-3.2.1.zip .
+7z x "glm-0.9.9.8.zip" -oglm-0.9.9.8
+7z x "glfw-3.2.1.zip" -oglfw-3.2.1
+
 
 echo "Building glfw"
-cd glfw-3.2.1
+cd ~/glfw-3.2.1/glfw-3.2.1/
 sudo cmake -G "Unix Makefiles"
 sudo make
 sudo make install
 
 echo "Building glm"
-cd ../glm/
+cd ~/glm-0.9.9.8/glm/
 sudo cmake -G "Unix Makefiles"
 sudo make
 sudo make install
 
 echo "Building SOIL2"
-cd ../SOIL2
+cd ~/SOIL2/
 premake5 gmake
 cd make/linux
 make
