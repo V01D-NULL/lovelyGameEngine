f245a2f27197aa595739e4c1221dafacfa7183ec
Parrot Live user <user@localhost.localdomain>
2021-02-15 20:36:52 +0000

    Added new UI/GUI library

--- nanogui/build/CMakeFiles/nanogui-python.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "python/nanogui.pdb"
-  "python/nanogui.so"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C CXX)
-  include(CMakeFiles/nanogui-python.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- scripts/install-deps-debian-linux.sh
+++ scripts/install-deps-debian-linux.sh
@@ -1,6 +1,6 @@
 # Install dependencies
 sudo apt update
-sudo apt install xorg-dev libglu1-mesa-dev libglfw3-dev libsoil-dev libglm-dev cmake libglew-dev libstb-dev libeigen3-dev python-dev -y
+sudo apt install xorg-dev libglu1-mesa-dev libglfw3-dev libsoil-dev libglm-dev cmake libglew-dev -y
 
 cd ..
 
@@ -18,15 +18,3 @@ sudo cmake -G "Unix Makefiles"
 sudo make
 sudo make install
 popd
-
-# Build nanogui
-echo "Building nanogui"
-mkdir lovelyGameEngine/nanogui/build
-pushd lovelyGameEngine/nanogui/build
-cmake ..
-make
-sudo make install
-sudo cp "../ext/nanovg/src/nanovg.h" "/usr/include/nanovg.h"
-popd
-
-sudo mv "/usr/include/eigen3/Eigen" "/usr/include/Eigen"
