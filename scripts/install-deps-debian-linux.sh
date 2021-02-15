0ea0a08450f35116e3b7013d5556e8043543f56f
Parrot Live user <user@localhost.localdomain>
2021-02-15 16:37:41 +0000

    Added nanogui lib

--- scripts/install-deps-debian-linux.sh
+++ scripts/install-deps-debian-linux.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install xorg-dev libglu1-mesa-dev libglfw3-dev libsoil-dev libglm-dev cmake libglew-dev -y
+sudo apt install xorg-dev libglu1-mesa-dev libglfw3-dev libsoil-dev libglm-dev cmake libglew-dev libstb-dev libeigen3-dev -y
 
 cd ..
 
@@ -9,7 +9,17 @@ cp lovelyGameEngine/ZIP_FILES/glfw-3.2.1.zip .
 
 
 echo "Building glfw"
-cd ~/glfw-3.2.1/glfw-3.2.1/
+pushd ~/glfw-3.2.1/glfw-3.2.1/
 sudo cmake -G "Unix Makefiles"
 sudo make
 sudo make install
+popd
+
+echo "Building nanogui"
+pushd nanogui/build
+cmake ..
+make nanogui
+sudo make install
+popd
+
+echo "NOTE: You have to move /usr/include/eigen3/Eigen to /usr/include/Eigen in order for nanogui to work"
