633d03857ed80f880312fd328cbd9428ec0b17cd
Parrot Live user <user@localhost.localdomain>
2021-02-15 16:42:04 +0000

    Updated to GLFW 3.3.2 & improved debian installation script

--- scripts/install-deps-debian-linux.sh
+++ scripts/install-deps-debian-linux.sh
@@ -1,20 +1,25 @@
+# Install dependencies
 sudo apt update
 sudo apt install xorg-dev libglu1-mesa-dev libglfw3-dev libsoil-dev libglm-dev cmake libglew-dev libstb-dev libeigen3-dev -y
 
 cd ..
 
+# Build GLFW
+GLFW_VER="3.3.2"
+
 echo "Unpacking zip file(s)"
-cp lovelyGameEngine/ZIP_FILES/glfw-3.2.1.zip .
-7z x "glfw-3.2.1.zip" -oglfw-3.2.1
+cp lovelyGameEngine/ZIP_FILES/glfw-$GLFW_VER.zip .
+7z x "glfw-$GLFW_VER.zip" -oglfw-$GLFW_VER
 
 
 echo "Building glfw"
-pushd ~/glfw-3.2.1/glfw-3.2.1/
+pushd ~/glfw-$GLFW_VER/glfw-$GLFW_VER/
 sudo cmake -G "Unix Makefiles"
 sudo make
 sudo make install
 popd
 
+# Build nanogui
 echo "Building nanogui"
 pushd nanogui/build
 cmake ..
